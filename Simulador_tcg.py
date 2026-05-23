1a45bf2455dd3584ad70e5da3a3719c14f08ccc3
BuenoSaaS <rafaelmoussalle@gmail.com>
2026-05-23 16:18:40 -0300

    Feat: Adicionadas funções para cálculo de perda de vida por ataque e outra que exibe a situação dos monstros ao usuário a cada turno

--- Simulador_tcg.py
+++ Simulador_tcg.py
@@ -20,3 +20,22 @@ while True:
   except ValueError:
     print("Insira valores numéricos para os pontos de vida e ataque, reproduza novamente o programa")
     break
+
+# Funções essenciais para o desenvolvimento da luta
+## Função de ataque, que recebe os dados dos inputs e mostra o resultado do ataque, além de calcular o hp restante do defensor  
+def Atacar(nome_atacante, nome_defensor, Ataque, hp):
+  ''' Função que recebe diretamente os dados dos inputs e mostra ao usuário quem ataca e quanto de dano irá causar, além de calcular o hp que resta'''
+  hp_defensor = hp - Ataque
+
+  if hp_defensor <= 0:
+    hp_defensor = 0
+  print(f"\n{nome_atacante} atingiu {nome_defensor} e causou {Ataque} pontos de dano!")
+  return hp_defensor
+
+### Função que exibe o placar a cada turno com o hp atualizado dos monstros
+def exibir_placar(Monstro_1, Monstro_2, hp_1, hp_2):
+  ''' Função que recebe os dados de hp atualizados e exibe como está '''
+  print("\n Fim do turno, aqui estão os resultados de ataque")
+  print(f"{Nome_monstro} possui {hp_1} pontos de vida")
+  print(f"{Nome_monstro_2} possui {hp_2} pontos de vida restantes")
+  return
